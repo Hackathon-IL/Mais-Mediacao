1e012260cc77d399a16466c2a09a136466efa176
Gustavo Marques <gustavo.marques@vtex.com.br>
2021-05-05 17:30:21 -0300

    Addition of emergencial aid screens

--- lua/navigation/regular/EmergencialAidStackNavigator.tsx
+++ lua/navigation/regular/EmergencialAidStackNavigator.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { createStackNavigator } from '@react-navigation/stack'
 import EmergencialAidScreen from "../../screens/regular/EmergencialAidScreen";
+import TutorialScreen from "../../screens/regular/emergencial_aid/TutorialScreen";
+import MapScreen from "../../screens/regular/emergencial_aid/MapScreen";
+import FaqScreen from "../../screens/regular/emergencial_aid/FaqScreen";
 
 export type EmergencialAidStackNavigatorParamsList = {
     InitialPage: undefined
+    Tutorial: undefined
+    Map: undefined
+    Faq: undefined
 }
 
 const EmergencialAidStack = createStackNavigator<EmergencialAidStackNavigatorParamsList>();
@@ -12,6 +18,9 @@ export default () => {
     return (
         <EmergencialAidStack.Navigator headerMode="none">
             <EmergencialAidStack.Screen name="InitialPage" component={EmergencialAidScreen} />
+            <EmergencialAidStack.Screen name="Tutorial" component={TutorialScreen} />
+            <EmergencialAidStack.Screen name="Map" component={MapScreen} />
+            <EmergencialAidStack.Screen name="Faq" component={FaqScreen} />
         </EmergencialAidStack.Navigator>
     );
 }
