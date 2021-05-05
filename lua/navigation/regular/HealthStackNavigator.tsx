ebeaac8dea8e989e32e359c6b3925d90bd47ffab
Gustavo Marques <gustavo.marques@vtex.com.br>
2021-05-05 17:24:19 -0300

    Creation of health stack

--- lua/navigation/regular/HealthStackNavigator.tsx
+++ lua/navigation/regular/HealthStackNavigator.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { createStackNavigator } from '@react-navigation/stack'
 import HealthScreen from "../../screens/regular/HealthScreen";
+import CovidScreen from "../../screens/regular/health/CovidScreen";
+import FaqScreen from "../../screens/regular/health/FaqScreen";
+import MapScreen from "../../screens/regular/health/MapScreen";
+import SymptomScreen from "../../screens/regular/health/SymptomScreen";
 
 export type HealthStackNavigatorParamsList = {
     InitialPage: undefined
+    Covid: undefined
+    Faq: undefined
+    Map: undefined
+    Symptom: undefined
 }
 
 const HealthStack = createStackNavigator<HealthStackNavigatorParamsList>();
@@ -12,6 +20,10 @@ export default () => {
     return (
         <HealthStack.Navigator headerMode="none">
             <HealthStack.Screen name="InitialPage" component={HealthScreen} />
+            <HealthStack.Screen name="Covid" component={CovidScreen} />
+            <HealthStack.Screen name="Faq" component={FaqScreen} />
+            <HealthStack.Screen name="Map" component={MapScreen} />
+            <HealthStack.Screen name="Symptom" component={SymptomScreen} />
         </HealthStack.Navigator>
     );
 }
