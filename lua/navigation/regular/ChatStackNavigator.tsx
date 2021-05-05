779d1eddb1c1274c9ef36943c80ef07f9679bf57
Gustavo Marques <gustavo.marques@vtex.com.br>
2021-05-05 17:34:55 -0300

    Creation of chat screens

--- lua/navigation/regular/ChatStackNavigator.tsx
+++ lua/navigation/regular/ChatStackNavigator.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { createStackNavigator } from '@react-navigation/stack'
 import ChatScreen from "../../screens/regular/ChatScreen";
+import DeafScreen from "../../screens/regular/chat/DeafScreen";
+import ImmigrantScreen from "../../screens/regular/chat/ImmigrantScreen";
+import IndigenousScreen from "../../screens/regular/chat/IndigenousScreen";
 
 export type ChatStackNavigatorParamsList = {
     InitialPage: undefined
+    Deaf: undefined
+    Immigrant: undefined
+    Indigenous: undefined
 }
 
 const ChatStack = createStackNavigator<ChatStackNavigatorParamsList>();
@@ -12,6 +18,9 @@ export default () => {
     return (
         <ChatStack.Navigator headerMode="none">
             <ChatStack.Screen name="InitialPage" component={ChatScreen} />
+            <ChatStack.Screen name="Deaf" component={DeafScreen} />
+            <ChatStack.Screen name="Immigrant" component={ImmigrantScreen} />
+            <ChatStack.Screen name="Indigenous" component={IndigenousScreen} />
         </ChatStack.Navigator>
     );
 }
