d6229d776f8ea35608582edfb99be50c228f3851
Gustavo Marques <gustavo.marques@vtex.com.br>
2021-05-08 17:06:05 -0300

    Addition of SupportScreen

--- lua/screens/regular/management/SupportScreen.tsx
+++ lua/screens/regular/management/SupportScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, ScrollView, useColorScheme, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, useColorScheme, StyleSheet, Image, Linking } from 'react-native';
 import { Button, TinyButton } from '../../../components/CommonComponents';
 import { commonStyles } from '../../../styles/common.styles';
 import { t } from 'i18n-js';
@@ -12,58 +12,105 @@ export type SupportScreenProps = {
 }
 
 export default ({ navigation }: SupportScreenProps) => {
-  const colorScheme = useColorScheme();
   const styles = commonStyles();
+  const IMI_CONSTITUTION_LINK = 'http://www.planalto.gov.br/ccivil_03/constituicao/constituicao.htm'
+  const IMI_MOBILANG_LINK = 'http://www.mobilang.unb.br/'
+  const IMI_ITAMARATY_LINK = 'http://www.portalconsular.itamaraty.gov.br/apoio-ao-imigrante'
+  const DEAF_CONSTITUTION_LINK = 'http://www.planalto.gov.br/ccivil_03/constituicao/constituicao.html'
+  const DEAF_PORTAL_LINK = 'https://www.libras.com.br/'
+  const IND_CONSTITUTION_LINK = 'http://www.planalto.gov.br/ccivil_03/constituicao/constituicao.html'
+  const IND_FUNAI_LINK = 'https://www.gov.br/funai/pt-br'
+
   return (
     <View style={styles.screen}>
       <Header
-        title={`${t('Theme')}: ${t(colorScheme || '')}`}
+        title="Apoio"
         hasBackButton={true}
         onBackPress={navigation.goBack}
       />
       <ScrollView style={styles.contentContainer}>
-        <View style={styles.bigCardContainer}>
-          <Text style={styles.mediumPrimaryText}> Big Card </Text>
+        <View style={local_styles.contentContainer}>
+          <View>
+            <View style={local_styles.labelContainer}>
+              <Image source={require('../../../assets/images/immigrant.png')} style={local_styles.icon} />
+              <Text style={[styles.mediumPrimaryText]}> Imigrantes </Text>
+            </View>
+            <View style={styles.horizontalSeparator} />
+          </View>
+          <Button
+            text="Constituição Brasileira"
+            style={local_styles.button}
+            onPress={() => Linking.openURL(IMI_CONSTITUTION_LINK)}
+          />
+          <Button
+            text="Mobilang"
+            style={local_styles.button}
+            onPress={() => Linking.openURL(IMI_MOBILANG_LINK)}
+          />
+          <Button
+            text="Itamaraty"
+            style={local_styles.button}
+            onPress={() => Linking.openURL(IMI_ITAMARATY_LINK)}
+          />
         </View>
-        <View style={styles.mediumCardContainer}>
-          <Text style={styles.smallPrimaryText}> Medium Card </Text>
+        <View style={local_styles.contentContainer}>
+          <View>
+            <View style={local_styles.labelContainer}>
+              <Image source={require('../../../assets/images/deaf.png')} style={local_styles.icon} />
+              <Text style={[styles.mediumPrimaryText]}> Surdos </Text>
+            </View>
+            <View style={styles.horizontalSeparator} />
+          </View>
+          <Button
+            text="Constituição Brasileira"
+            style={local_styles.button}
+            onPress={() => Linking.openURL(DEAF_CONSTITUTION_LINK)}
+          />
+          <Button
+            text="Portal LIBRAS"
+            style={local_styles.button}
+            onPress={() => Linking.openURL(DEAF_PORTAL_LINK)}
+          />
         </View>
-        <View style={[styles.smallCardContainer, { flexDirection: 'row' }]}>
-          <Text style={styles.tinyPrimaryText}> Small Card </Text>
-          <View style={styles.verticalSeparator} />
-          <Text style={styles.tinyPrimaryText}> Small Card </Text>
+        <View style={local_styles.contentContainer}>
+          <View>
+            <View style={local_styles.labelContainer}>
+              <Image source={require('../../../assets/images/indigenous.png')} style={local_styles.icon} />
+              <Text style={[styles.mediumPrimaryText]}> Indígenas </Text>
+            </View>
+            <View style={styles.horizontalSeparator} />
+          </View>
+          <Button
+            text="Constituição Brasileira"
+            style={local_styles.button}
+            onPress={() => Linking.openURL(IND_CONSTITUTION_LINK)}
+          />
+          <Button
+            text="FUNAI"
+            style={local_styles.button}
+            onPress={() => Linking.openURL(IND_FUNAI_LINK)}
+          />
         </View>
-        <View style={styles.tinyCardContainer}>
-          <Text style={styles.tinyPrimaryText}> Tiny Card </Text>
-        </View>
-
-        <View style={styles.horizontalSeparator} />
-
-        <Text style={styles.bigPrimaryText}>Big primary text </Text>
-        <Text style={styles.mediumPrimaryText}>Medium primary text </Text>
-        <Text style={styles.smallPrimaryText}>Small primary text </Text>
-        <Text style={styles.tinyPrimaryText}>Tiny primary text </Text>
-
-        <Text style={styles.bigSecondaryText}>Big secondary text </Text>
-        <Text style={styles.mediumSecondaryText}>Medium secondary text </Text>
-        <Text style={styles.smallSecondaryText}>Small secondary text </Text>
-        <Text style={styles.tinySecondaryText}>Tiny secondary text </Text>
-
-        <Text style={styles.bigTertiaryText}>Big tertiary text </Text>
-        <Text style={styles.mediumTertiaryText}>Medium tertiary text </Text>
-        <Text style={styles.smallTertiaryText}>Small tertiary text </Text>
-        <Text style={styles.tinyTertiaryText}>Tiny tertiary text </Text>
-
-        <Button
-          text={"Button"}
-        />
-        <TinyButton text={"Tiny button"} />
       </ScrollView>
     </View>
   );
 }
 const local_styles = StyleSheet.create({
   mainContainer: {
-      justifyContent: 'center',
+    justifyContent: 'center',
+  },
+  contentContainer: {
+    padding: 10
+  },
+  labelContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end'
+  },
+  icon: {
+    height: 30,
+    width: 30
+  },
+  button: {
+    width: '100%'
   }
 })
